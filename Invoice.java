cc34413db13a6ecf2dff8e5984fc37076f9452d1
Naech <empnaechark@gmail.com>
2019-05-31 10:01:09 +0100

    added a file

--- Invoice.java
+++ Invoice.java
@@ -7,10 +7,20 @@ public class Invoice{
     private double Price;
 
     public Invoice(String PartNumber, String PartDescription, int ItemQuantity, double Price){
-         PartNumber = number;
-         PartDescription = description;
-         ItemQuantity = quantity;
-         Price = price;
+         this.ItemQuantity = ItemQuantity;
+         this.Price = Price;
+         if(ItemQuantity < 0){
+             this.ItemQuantity = 0;
+         }
+         if(ItemQuantity > 0){
+             this.ItemQuantity = ItemQuantity;
+         }
+         if(Price < 0){
+             this.Price = 0;
+         }
+         if(Price > 0){
+             this.Price = Price;
+         }
     }
 
     public void setPartNumber(String PartNumber){
@@ -20,7 +30,7 @@ public class Invoice{
         return PartNumber;
     }
 
-    public void setpartDescription(String PartDescription){
+    public void setPartDescription(String PartDescription){
         this.PartDescription = PartDescription;
     }
     public String getPartDescription(){
@@ -46,4 +56,6 @@ public class Invoice{
         calculateInvoiceAmount = ItemQuantity * Price;
         return calculateInvoiceAmount;
     }
+
+       
 }
