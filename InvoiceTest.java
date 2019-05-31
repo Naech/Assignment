8e2fbcf7ac2bd6e104251daff9854042ee4f86a1
Naech <empnaechark@gmail.com>
2019-05-31 10:02:40 +0100

    added a file

--- InvoiceTest.java
+++ InvoiceTest.java
@@ -2,38 +2,34 @@ import java.util.Scanner;
 
 public class InvoiceTest{
     
-    private String number;
-    private String description;
-    private int quantity;
-    private double price;
 
     public static void main(String args[]){
-       Invoice input = new Invoice(" ", " ", 0, 0.0);
+       Invoice invoice = new Invoice(" ", " ", 0, 0.0);
 
        Scanner input = new Scanner(System.in);
 
        System.out.print("Enter Invoice Number: ");
-       number = input.nextLine();
-       invoice.setPartNumber(number);
+       String PartNumber = input.nextLine();
+       invoice.setPartNumber(PartNumber);
 
        System.out.print("Enter Invoice Description: ");
-       description = input.nextLine();
-       invoice.setPartDescription(description);
+       String PartDescription = input.nextLine();
+       invoice.setPartDescription(PartDescription);
       
        System.out.print("Enter Quantity: ");
-       quantity = input.nextInt();
-       if(quantity >= 0){
-           invoice.setItemQuantity(quantity);
+       int ItemQuantity = input.nextInt();
+       if(ItemQuantity >= 0){
+       invoice.setItemQuantity(ItemQuantity);
        }
 
-       System.out.print("Enter Price");
-       price = input.nextDouble();
-       if(price >= 0.0){
-       invoice.setPrice(price);
-
+       System.out.print("Enter Price: ");
+       double Price = input.nextDouble();
+       if(Price >= 0.0){
+       invoice.setPrice(Price);
        }
 
-       System.out.printf("The Invoice: $%2.f%n);
-          invoice.getInvoiceMethod();
+       System.out.println(invoice.getInvoiceAmount());
+       
+
     }
 }
