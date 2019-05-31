5e1b2b3be6437d0eb502fd3395ac53a2927e9297
Naech <empnaechark@gmail.com>
2019-05-31 10:12:56 +0100

    added a date test

---
+++ DateTest.java
@@ -0,0 +1,8 @@
+public class DateTest{
+
+    public static void main(String args[]){
+        Date date = new Date(29, 05, 2019);
+
+        System.out.println("Wow today is: " + date.displayDate());
+    }
+}
